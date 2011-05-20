61eb550011499f26cb8285eb0dcfa71fbe00791c
Patrick Strang <strangnet@gmail.com>
2011-05-20 23:54:09 +0200

    autentisering

--- src/se/su/dsv/scipro/android/activity/Authenticate.java
+++ src/se/su/dsv/scipro/android/activity/Authenticate.java
@@ -2,6 +2,8 @@ package se.su.dsv.scipro.android.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 import se.su.dsv.scipro.android.R;
 
 /**
@@ -12,6 +14,10 @@ import se.su.dsv.scipro.android.R;
 public class Authenticate extends Activity {
 
     public static final String TAG = "Authenticate";
+    private TextView usernameText;
+    private TextView passwordText;
+    private TextView apikeyText;
+    private Button authenticateButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +27,9 @@ public class Authenticate extends Activity {
     }
 
     private void setUpViews() {
-
+        usernameText = (TextView) findViewById(R.id.auth_username_entry);
+        passwordText = (TextView) findViewById(R.id.auth_password_entry);
+        apikeyText = (TextView) findViewById(R.id.auth_apikey_entry);
+        authenticateButton = (Button) findViewById(R.id.auth_button);
     }
 }
