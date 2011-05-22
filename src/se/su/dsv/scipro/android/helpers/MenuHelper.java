5999f4c97a26162a749ab4235c27e4498552810f
Patrick Strang <strangnet@gmail.com>
2011-05-22 03:56:54 +0200

    new menu alternative: logout

--- src/se/su/dsv/scipro/android/helpers/MenuHelper.java
+++ src/se/su/dsv/scipro/android/helpers/MenuHelper.java
@@ -1,11 +1,12 @@
 package se.su.dsv.scipro.android.helpers;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.view.MenuItem;
 import se.su.dsv.scipro.android.Preferences;
 import se.su.dsv.scipro.android.R;
+import se.su.dsv.scipro.android.SciProApplication;
+import se.su.dsv.scipro.android.activity.MainActivity;
 
 /**
  * User: patrick
@@ -14,15 +15,16 @@ public class MenuHelper {
 
     public static boolean openActivityFromMenuItem(Context context, MenuItem item) {
         Intent intent;
-        Class<? extends Activity> requestingClass = (Class<? extends Activity>) context.getClass();
         switch (item.getItemId()) {
             case R.id.preferences_menu_item:
-                if (requestingClass != Preferences.class) {
                     intent = new Intent(context, Preferences.class);
                     context.startActivity(intent);
                     return true;
-                }
-                break;
+            case R.id.logout_menu_item:
+                SciProApplication.getInstance().logout();
+                intent = new Intent(context, MainActivity.class);
+                context.startActivity(intent);
+                return true;
         }
         return false;
     }
